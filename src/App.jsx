a55fcedd93a4d50913141cbc9b8acc426c94c74c
Belén <belen.munoz@usach.cl>
2024-07-05 18:59:09 -0400

    fix: limpieza App.jsx
    - App.jsx vacío para hacer las pruebas.
    - En el commit anterior, se hizo bosquejo inicial de vista Categoría.jsx y su Card correspondiente. Además, se editó index.css con atributos para body y cambio en color background card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import Categoria from "./pages/Categoria/Categoria";
-const categoria_nombre = "Electricidad";
 
 function App() {
-  return (
-    <>
-      <Categoria categoria_nombre={categoria_nombre} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
