f34e44ec09a3f43b5036e9ced8cc5e96c560d797
Catabel8 <c.espinoza.leiva@gmail.com>
2024-07-09 15:02:12 -0400

    acercaDe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import AcercaDe from './pages/AcercaDe/AcercaDe';
+
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <AcercaDe />
+    </div>
+  );
 }
 
 export default App;
