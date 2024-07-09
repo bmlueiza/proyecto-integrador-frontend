39f7477f2ac91c390fffb056198b515c337a27dd
pLoayza <ped.loayza@duocuc.cl>
2024-07-09 14:59:27 -0400

    Agregado Navbar, SearchBar y SimpleMenu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./Navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "../../assets/logo.png";
+import SearchAppBar from "../SearchBar/SearchBar";
+import BasicMenu from "../SimpleMenu/SimpleMenu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="nav">
+        <div className="nav-logo">
+          <a href="">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>
+              <a href="#">Minga</a>
+            </li>
+            <li>
+              <a href="#">Categorias</a>
+            </li>
+            <li>
+              <a href="#">Mensajes</a>
+            </li>
+            <SearchAppBar className="searchbar" />
+            {/* <li className="menu">
+              <a href="#">
+                <MenuIcon />
+              </a>
+            </li> */}
+          </ul>
+        </div>
+        <BasicMenu className="prueba" />
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
---
+++ src/components/SimpleMenu/SimpleMenu.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import "./SimpleMenu.css";
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        className="prueba"
+      >
+        <AccountCircleIcon />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <div className="dropdown-text">
+          <MenuItem onClick={handleClose}>Configuracion</MenuItem>
+          <MenuItem onClick={handleClose}>Cerrar sesion</MenuItem>
+        </div>
+      </Menu>
+    </div>
+  );
+}
