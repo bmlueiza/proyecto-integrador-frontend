e9208e1badb278f481e981c2fbdd2475d70aefe0
Belén <belen.munoz@usach.cl>
2024-07-11 00:06:49 -0400

    fix: ubicación css de Buton.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,19 @@
+import "./Button.css";
 import PropTypes from "prop-types";
 
-const Button = ({ text = null, icon = null, onClick }) => {
+function Button({ text = null, icon = null, onClick, className = "" }) {
   return (
-    <button onClick={onClick} className="btn">
+    <button onClick={onClick} className={`btn ${className}`}>
       {icon && <span className="button-icon">{icon}</span>}
       {text && <span className="button-text">{text}</span>}
     </button>
   );
-};
-
+}
 Button.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.element,
   onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
 };
 
 export default Button;
