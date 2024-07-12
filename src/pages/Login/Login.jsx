1f00e5f76f9ae5537c3e40758bdd662c271259c9
Vicente <montoya.olguin.v@gmail.com>
2024-07-11 20:37:17 -0400

    se agregan cambios jsx y css de pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import LoginForm from "../../components/Forms/LoginForm/LoginForm";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <Navbar />
+      <h1 className="title_home">Minga</h1>
+      <hr className="divider" />
+      <LoginForm />
+      <Footer />
+    </>
+  );
 }
 
 export default Login;
--- src/pages/RegistroUser/RegistroUser.jsx
+++ src/pages/RegistroUser/RegistroUser.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import RegisterProfessional from "../../components/Forms/RegistroFormProfessional/RegistroFormProfessional";
 
 function RegistroUser() {
-  return <div>RegistroUser</div>;
+  return (
+    <>
+      <Navbar />
+      <h1 className="title_home">Minga</h1>
+      <hr className="divider" />
+      <RegisterProfessional />
+      <Footer />
+    </>
+  );
 }
 
 export default RegistroUser;
