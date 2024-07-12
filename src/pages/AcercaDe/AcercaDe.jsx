5b4047bdfea037c1df6706c3a009c8c472fcba72
Belén <belen.munoz@usach.cl>
2024-07-11 20:05:49 -0400

    fix: doble import react AcercaDe

--- src/pages/AcercaDe/AcercaDe.jsx
+++ src/pages/AcercaDe/AcercaDe.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import logo from '../../assets/logo.png'; // Importa el logo
-import './AcercaDe.css';
 import React from "react";
 import logo from "../../assets/logo.png"; // Importa el logo
-import "./AcercaDe.css"; // Asegúrate de tener la hoja de estilos en el mismo directorio
+import "./AcercaDe.css";
 
 const AcercaDe = () => {
   return (
@@ -14,11 +11,23 @@ const AcercaDe = () => {
       </div>
       <div className="contenido">
         <h2>¿Quiénes somos?</h2>
-        <p>Minga es una aplicación web para conectar trabajadores domésticos con clientes que requieren su servicio.</p>
+        <p>
+          Minga es una aplicación web para conectar trabajadores domésticos con
+          clientes que requieren su servicio.
+        </p>
         <h2>Misión</h2>
-        <p>Nuestra misión es facilitar la conexión entre trabajadores como carpinteros, albañiles, pintores y otros profesionales del hogar con personas que necesitan sus servicios, asegurando calidad y confianza en cada trabajo realizado.</p>
+        <p>
+          Nuestra misión es facilitar la conexión entre trabajadores como
+          carpinteros, albañiles, pintores y otros profesionales del hogar con
+          personas que necesitan sus servicios, asegurando calidad y confianza
+          en cada trabajo realizado.
+        </p>
         <h2>Visión</h2>
-        <p>Nuestra visión es ser la plataforma líder en la región para la contratación de servicios domésticos, reconocida por la excelencia y la satisfacción tanto de los trabajadores como de los clientes.</p>
+        <p>
+          Nuestra visión es ser la plataforma líder en la región para la
+          contratación de servicios domésticos, reconocida por la excelencia y
+          la satisfacción tanto de los trabajadores como de los clientes.
+        </p>
       </div>
     </div>
   );
